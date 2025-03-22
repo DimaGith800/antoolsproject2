2d9a8981c0c859c7c62f5c36a20a9766cbe674e5
DimaGith800 <dm.oleynichenko@gmail.com>
2025-03-22 07:39:10 +0200

    antools

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('visitbuttonmanipulation');
+
+button.addEventListener('click', () => {
+    button.classList.toggle('active'); // Добавляет или убирает класс при клике
+});
